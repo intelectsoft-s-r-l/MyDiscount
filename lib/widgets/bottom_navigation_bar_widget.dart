51c8fadefa38304b59dcf730df3e6a183f68b2e9
ioncristea <cristeaion36@gmail.com>
2021-01-06 17:38:47 +0200

    change bottom navigation bar

--- lib/widgets/bottom_navigation_bar_widget.dart
+++ lib/widgets/bottom_navigation_bar_widget.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:MyDiscount/new_button/ripple_animation.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -15,6 +18,7 @@ class BottomNavigationBarWidget extends StatefulWidget {
 class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget>
     with WidgetsBindingObserver {
   int selectedIndex = 1;
+  StreamController _indexController = StreamController();
   @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
@@ -22,17 +26,18 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget>
     super.initState();
   }
 
-  @override
+  /* @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-   
+    if(mounted)
     if (state == AppLifecycleState.resumed) {
-       Navigator.of(context).pushReplacementNamed('/app');
-    } 
+      Navigator.of(context).pushReplacementNamed('/app');
+    }
     super.didChangeAppLifecycleState(state);
-  }
+  } */
 
   @override
   void dispose() {
+    _indexController.close();
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
@@ -40,12 +45,111 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: [
-        HomePage(),
-        QrPage(),
-        NotificationPage(),
-      ].elementAt(selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
+
+        body: [
+          HomePage(),
+          QrPage(),
+          NotificationPage(),
+        ].elementAt(selectedIndex),
+        bottomNavigationBar: StreamBuilder(
+          stream: _indexController.stream,
+          builder: (context, snapshot) => Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  top: BorderSide(
+                    color: Colors.grey[200],
+                  ),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[200],
+                    blurRadius: 0.1,
+                    spreadRadius: 1.0,
+                  )
+                ]),
+            //color: Colors.red,
+            height: 60,
+            child: Row(
+              children: [
+                InkResponse(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = 0;
+                      _indexController.add(0);
+                    });
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .33,
+                    // color: Colors.green,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color:
+                              snapshot.data == 0 ? Colors.blue : Colors.black,
+                        ),
+                        Text(
+                          AppLocalizations.of(context).translate('text21'),
+                          style: TextStyle(
+                            color:
+                                snapshot.data == 0 ? Colors.blue : Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),        
+                Expanded(
+                  child: InkResponse(
+                    onTap: () {
+                      setState(() {
+                        _indexController.add(1);
+                        selectedIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      color: Colors.white,
+                      child: RipplesAnimation(),
+                    ),
+                  ),
+                ),
+                InkResponse(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = 2;
+                      _indexController.add(2);
+                    });
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .33,
+                    //color: Colors.amber,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                           MdiIcons.newspaper,
+                          color:
+                              snapshot.data == 2 ? Colors.blue : Colors.black,
+                        ),
+                        Text(
+                          AppLocalizations.of(context).translate('text23'),
+                          style: TextStyle(
+                            color:
+                                snapshot.data == 2 ? Colors.blue : Colors.black,
+                            //fontSize: snapshot.data == 2?15:10
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ) /* BottomNavigationBar(
         /* type: BottomNavigationBarType.shifting, */
         backgroundColor: Colors.white,
         unselectedFontSize: MediaQuery.of(context).devicePixelRatio + 6,
@@ -63,13 +167,13 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget>
                 color: Colors.black,
               ),
               label: AppLocalizations.of(context).translate('text21')),
-          BottomNavigationBarItem(
+          /* BottomNavigationBarItem(
             icon: Icon(
               MdiIcons.qrcode,
               color: Colors.black,
             ),
             label: 'QR',
-          ),
+          ), */
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.notifications,
@@ -77,7 +181,7 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget>
               ),
               label: AppLocalizations.of(context).translate('text23')),
         ],
-      ),
-    );
+      ), */
+        );
   }
 }
