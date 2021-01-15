056eafb0ea72bbad18a6e188d2964abc82b86755
ioncristea <cristeaion36@gmail.com>
2021-01-15 17:52:57 +0200

    verify news internet connection

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -7,6 +7,7 @@ import 'package:http/http.dart' as http;
 import '../core/constants/credentials.dart';
 import '../core/formater.dart';
 import '../models/news_model.dart';
+import '../services/internet_connection_service.dart';
 import '../services/shared_preferences_service.dart';
 import '../services/remote_config_service.dart';
 
@@ -14,41 +15,46 @@ class NewsService {
   SharedPref _prefs = SharedPref();
   Formater formater = Formater();
   Credentials credentials = Credentials();
+  NetworkConnectionImpl status = NetworkConnectionImpl();
   Box<News> newsBox = Hive.box<News>('news');
 
   Future<List<News>> getNews() async {
     if (await _prefs.readNewsState()) {
-      final serviceName = await getServiceNameFromRemoteConfig();
-      final id = await readEldestNewsId();
-      final url = '$serviceName/json/GetAppNews?ID=$id';
-      final response = await http.get(url, headers: credentials.header);
-      final Map<String, dynamic> decodedResponse = json.decode(response.body);
-      
-      final list = decodedResponse['NewsList'] as List;
-      final parseDate =
-          formater.parseDateTimeAndSetExpireDate(list, 'CreateDate');
-      final dat = formater.checkImageFormatAndSkip(parseDate, 'CompanyLogo');
+      if (await status.isConnected) {
+        final serviceName = await getServiceNameFromRemoteConfig();
+        final id = await readEldestNewsId();
+        final url = '$serviceName/json/GetAppNews?ID=$id';
+        final response = await http.get(url, headers: credentials.header);
+        final Map<String, dynamic> decodedResponse = json.decode(response.body);
 
-      final dataList = formater.checkImageFormatAndSkip(dat, 'Photo');
-      dataList
-          .map((e) => News.fromJson(e))
-          .toList()
-          .forEach((element) => saveNewsOnDB(element));
-      return _getReversedNewsList();
+        final list = decodedResponse['NewsList'] as List;
+        final parseDate =
+            formater.parseDateTimeAndSetExpireDate(list, 'CreateDate');
+        final dat = formater.checkImageFormatAndSkip(parseDate, 'CompanyLogo');
+
+        final dataList = formater.checkImageFormatAndSkip(dat, 'Photo');
+        dataList
+            .map((e) => News.fromJson(e))
+            .toList()
+            .forEach((element) => saveNewsOnDB(element));
+        return _getReversedNewsList();
+      } else {
+        if (newsBox.isNotEmpty) {
+          return _getReversedNewsList();
+        }
+      }
     } else {
       final keys = newsBox.keys;
       if (newsBox.isNotEmpty) newsBox.deleteAll(keys);
-      
     }
     return [];
   }
 
   Future<String> readEldestNewsId() async {
     final listOfKeys = newsBox.keys;
-    
+
     int id = 0;
     if (listOfKeys.isNotEmpty)
-      
       for (int key in listOfKeys)
         if (key > id) {
           id = key;
