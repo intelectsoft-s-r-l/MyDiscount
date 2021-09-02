892dc8a870066bc4f29e5748f6fa0b10c6cc49e7
Cristea <cristeaion36@gmail.com>
2021-09-02 14:11:39 +0300

    fix bug on profile phone validation

--- lib/presentation/widgets/profile_page_widgets/phone_validation_widget.dart
+++ lib/presentation/widgets/profile_page_widgets/phone_validation_widget.dart
@@ -92,6 +92,7 @@ class _ValidatePhoneFormWidgetState extends State<ValidatePhoneFormWidget> {
                                 initialPhoneNumber:
                                     profile.phone.characters.skip(4).toString(),
                                 initialSelection: phoneIsoCode,
+                                isValidPhoneNumber:(valid){},
                                 enabledCountries: {
                                   '+40': 'RO',
                                   '+7': 'RU',
